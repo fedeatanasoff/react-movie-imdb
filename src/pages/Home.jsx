96f25109096ee72f9e27b75d5d2df46ad9fb95e8
fedeatanasoff <fede.atanasoff@gmail.com>
2019-04-07 13:33:03 -0300

    consumiendo api desde home con axios y async await

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,53 @@
 import React, { Component } from "react";
 import Destacados from "../components/Destacados";
 import Estrenos from "../components/Estrenos";
+import axios from "axios";
 
 class Home extends Component {
+  state = {
+    peliculas: []
+  };
+
+  componentDidMount() {
+    if (this.state.peliculas.length === 0) {
+      this.getData()
+        .then(data => this.setState({ peliculas: data.data.results }))
+        .catch(e => console.log("Error: ", e));
+    }
+  }
+
+  async getData() {
+    let resultado = await axios.get(
+      `https://api.themoviedb.org/3/movie/now_playing?api_key=26fcc7f157d992f403ae4b6144eabf3&language=es-AR`
+    );
+
+    return resultado;
+
+    // axios
+    //   .get(
+    //     `https://api.themoviedb.org/3/movie/now_playing?api_key=a26fcc7f157d992f403ae4b6144eabf3&language=es-AR`
+    //   )
+    //   .then(data => {
+    //     console.log(data);
+    //     this.setState({ peliculas: data.data.results });
+    //   });
+
+    // fetch(
+    //   `https://api.themoviedb.org/3/movie/now_playing?api_key=a26fcc7f157d992f403ae4b6144eabf3&language=es-AR`
+    // )
+    //   .then(res => res.json())
+    //   .then(data => {
+    //     console.log("data fetch", data);
+    //     this.setState({ peliculas: data.results });
+    //   });
+  }
+
   render() {
+    console.log("estado desde el render =>", this.state.peliculas);
     return (
       <div>
         <Destacados />
-        <Estrenos />
+        <Estrenos peliculas={this.state.peliculas} />
       </div>
     );
   }
